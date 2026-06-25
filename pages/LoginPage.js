52439c8e395fd0e55b3ff9928b4000c4fddac77e
Dhananjayunde <77325569+Dhananjayunde@users.noreply.github.com>
2026-06-25 17:47:16 +0530

    changes for login page

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -33,7 +33,7 @@ class LoginPage {
         await this.page.waitForLoadState('networkidle');
     }
 
-    async enterOTP(otpCode = '062707') {
+    async enterOTP(otpCode = '976666') {
         const otpDigits = otpCode.split('');
         const otpLocators = ['_r_3_', '_r_4_', '_r_5_', '_r_6_', '_r_7_', '_r_8_'];
         
